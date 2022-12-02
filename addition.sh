918f73518bf5800624839371f2c8fdc849654d7a
NayanTulupuleYTI <Nayan.Tulupule@yokogawa.com>
2022-12-02 13:17:44 +0530

    hogaya

---
+++ addition.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function addition {
+  local FIRST=$1
+  local SECOND=$2
+  let RESULT=FIRST+SECOND
+  echo "RESULT is: $RESULT"
+  let FIRST++
+  let SECOND++
+  }
+echo -n "Enter first number:"
+read FIRST
+echo -n "Enter second number:"
+read SECOND
+addition $FIRST $SECOND
+
+echo "Printing variables:"
+echo "FIRST: $FIRST"
+echo "SECOND: $SECOND"
